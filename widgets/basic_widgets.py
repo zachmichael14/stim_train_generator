3d816d521c1554c81dd2edf40a9432c119e5647d
zachmichael14 <zachmichael14@gmail.com>
2024-07-24 11:42:57 -0500

    Add basic repetition widget

--- widgets/basic_widgets.py
+++ widgets/basic_widgets.py
@@ -1,56 +1,55 @@
-from abc import abstractmethod
-from typing import Dict
-
 from PySide6 import QtWidgets
-from PySide6 import QtCore
+import numpy as np
 
 class ConstantWidget(QtWidgets.QWidget):
-    def __init__(self, label_text: str="Constant"):
+    def __init__(self):
         super().__init__()
+        self.text_edit = QtWidgets.QLineEdit()
+        self.init_ui()
+    
+    def init_ui(self):
         main_layout = QtWidgets.QHBoxLayout()
 
-        self.text_edit = QtWidgets.QLineEdit()
-        self.text_label = QtWidgets.QLabel(f"{label_text}: ")
+        text_label = QtWidgets.QLabel(f"Constant:")
 
-        main_layout.addWidget(self.text_label)
+        main_layout.addWidget(text_label)
         main_layout.addWidget(self.text_edit)
 
         self.setLayout(main_layout)
 
     def get_values(self):
-        return {"constant": self.text_edit.text()}
+        """Return 1-D numpy array."""
+        try:
+            return np.array(float(self.text_edit.text()))
+        except ValueError as e:
+            print(f"ConstantWidget Error: {e}")
 
 
 class RampWidget(QtWidgets.QWidget):
-    def __init__(self,
-                start_label="Start:",
-                stop_label="Stop:",
-                points_label="Number of points:"):
-        
-        super().__init__()        
-        main_layout = QtWidgets.QVBoxLayout()
-
-        self.start_label = QtWidgets.QLabel(start_label)
+    def __init__(self):
+        super().__init__()
         self.start_edit = QtWidgets.QLineEdit()
-        
-        start_layout = QtWidgets.QHBoxLayout()
-        start_layout.addWidget(self.start_label)
-        start_layout.addWidget(self.start_edit)
-
-        self.stop_label = QtWidgets.QLabel(stop_label)
         self.stop_edit = QtWidgets.QLineEdit()
-
-        stop_layout = QtWidgets.QHBoxLayout()
-        stop_layout.addWidget(self.stop_label)
-        stop_layout.addWidget(self.stop_edit)
-        
-        self.points_label = QtWidgets.QLabel(points_label)
         self.points_edit = QtWidgets.QLineEdit()
+        self.init_ui()
 
+    def init_ui(self):
+        main_layout = QtWidgets.QVBoxLayout()
+        start_layout = QtWidgets.QHBoxLayout()
+        stop_layout = QtWidgets.QHBoxLayout()
         points_layout = QtWidgets.QHBoxLayout()
-        points_layout.addWidget(self.points_label)
+
+        start_label = QtWidgets.QLabel("Start:")
+        stop_label = QtWidgets.QLabel("Stop:")
+        points_label = QtWidgets.QLabel("Number of Points:")
+
+        start_layout.addWidget(start_label)
+        start_layout.addWidget(self.start_edit)
+        stop_layout.addWidget(stop_label)
+        stop_layout.addWidget(self.stop_edit)
+        points_layout.addWidget(points_label)
         points_layout.addWidget(self.points_edit)
-        
+    
         main_layout.addLayout(start_layout)
         main_layout.addLayout(stop_layout)
         main_layout.addLayout(points_layout)
@@ -58,13 +57,32 @@ class RampWidget(QtWidgets.QWidget):
         self.setLayout(main_layout)
 
     def get_values(self):
-        return {
-            "start": self.start_edit.text(),
-            "stop": self.stop_edit.text(),
-            "points": self.points_edit.text()
-        }
+        """Return a numpy array of linearly spaced values."""
+        try:
+            start = float(self.start_edit.text())
+            stop = float(self.stop_edit.text())
+            points = int(self.points_edit.text())
 
+            return np.linspace(start, stop, points)
+        except ValueError as e:
+            print(f"RampWidget Error: {e}")
 
+    def get_raw_values_dict(self):
+        """Return a dictionary of user-entered values."""
+        try:
+            start = float(self.start_edit.text())
+            stop = float(self.stop_edit.text())
+            points = float(self.points_edit.text())
+
+            return {
+                "start": start,
+                "stop": stop,
+                "points": points,
+            }
+        except ValueError as e:
+            print(f"RampWidget Error: {e}")
+
+    
 class FunctionWidget(QtWidgets.QWidget):
     def __init__(self, options: list=["Option 1", "Option 2", "Option 3"]):
         super().__init__()
@@ -77,131 +95,23 @@ class FunctionWidget(QtWidgets.QWidget):
 
     def get_values(self):
         return {"function": self.function_dropdown.currentText()}
-        
 
-class IntervalWidget(QtWidgets.QWidget):
-    value_changed = QtCore.Signal(dict)
-    mode_changed = QtCore.Signal()
 
-    def __init__(self, title):
+class RepetitionWidget(QtWidgets.QWidget):
+    def __init__(self, label_text):
         super().__init__()
-        self.title = title
-        self.subwidget = None
+        self.label = QtWidgets.QLabel(label_text)
+        self.textbox = QtWidgets.QLineEdit()
         self.init_ui()
-        
+
     def init_ui(self):
-        self.main_layout = QtWidgets.QVBoxLayout(self)
-        self.mode_groupbox = QtWidgets.QGroupBox(f"{self.title}")
-        self.mode_layout = QtWidgets.QVBoxLayout(self.mode_groupbox)
-
-        self.init_mode_selectors()
-
-        self.subwidget_container = QtWidgets.QWidget()
-        self.subwidget_layout = QtWidgets.QVBoxLayout(self.subwidget_container)
-
-        # ConstantWidget is default
-        self.subwidget = ConstantWidget()
-        self.subwidget_layout.addWidget(self.subwidget)
-        self.connect_subwidget_signals(self.subwidget)
-
-        self.mode_layout.addWidget(self.subwidget_container)
-        self.main_layout.addWidget(self.mode_groupbox)
-        self.setLayout(self.main_layout)       
-
-    def init_mode_selectors(self):
-        selector_layout = QtWidgets.QHBoxLayout()
-
-        self.constant_button = QtWidgets.QRadioButton("Constant", self)
-        self.constant_button.setChecked(True)
-        self.ramp_button = QtWidgets.QRadioButton("Ramp", self)
-        self.function_button = QtWidgets.QRadioButton("Function", self)
-
-        self.mode_selector = QtWidgets.QButtonGroup(self)
-        self.mode_selector.addButton(self.constant_button)
-        self.mode_selector.addButton(self.ramp_button)
-        self.mode_selector.addButton(self.function_button)
-
-        self.mode_selector.buttonClicked.connect(self.handle_mode_selector)
-
-        selector_layout.addWidget(self.constant_button)
-        selector_layout.addWidget(self.ramp_button)
-        selector_layout.addWidget(self.function_button)
-
-        self.mode_layout.addLayout(selector_layout)
-        
-    def handle_mode_selector(self, button: QtWidgets.QRadioButton):
-        if button == self.constant_button:
-            self.show_widget(ConstantWidget)
-        elif button == self.ramp_button:
-            self.show_widget(RampWidget)
-        else:
-            self.show_widget(FunctionWidget)
-        self.mode_changed.emit()
-        
-
-    def show_widget(self, widget_class: QtWidgets.QWidget):
-        new_subwidget = widget_class()
-        self.subwidget_layout.replaceWidget(self.subwidget, new_subwidget)
-        self.subwidget.deleteLater()
-        self.subwidget = new_subwidget
-        self.connect_subwidget_signals(self.subwidget)
-
-    def connect_subwidget_signals(self, widget):
-        # Connect to the QLineEdit's textChanged signal
-        if isinstance(widget, (ConstantWidget, RampWidget)):
-            for child in widget.findChildren(QtWidgets.QLineEdit):
-                child.textChanged.connect(self.validate_and_emit)
-        
-        # Connect to the QComboBox's currentTextChanged signal
-        elif isinstance(widget, FunctionWidget):
-            widget.function_dropdown.currentTextChanged.connect(self.validate_and_emit)
-
-    def validate_and_emit(self):
-        # Emit the custom signal with the current values
-        values = self.get_values()
-        if self.validate_values(values):
-            self.value_changed.emit(values)
+        main_layout = QtWidgets.QHBoxLayout()
+        main_layout.addWidget(self.label)
+        main_layout.addWidget(self.textbox)
+        self.setLayout(main_layout)
 
     def get_values(self):
-        mode = self.get_current_mode()
-        subwidget_values = self.subwidget.get_values()
-        return {"mode": mode, "values": subwidget_values}
-    
-    def get_current_mode(self):
-        if self.constant_button.isChecked():
-            return "constant"
-        elif self.ramp_button.isChecked():
-            return "ramp"
-        else:
-            return "function"
-
-    def validate_values(self, values):
-        # Implement amplitude-specific validation
-        if values["mode"].lower() == "constant":
-            try:
-                interval = float(values["values"]["constant"])
-                # return 0 <= amplitude <= 100  # Example range
-                return True
-            except ValueError:
-                return False
-        elif values["mode"].lower() == "ramp":
-            try:
-                start = float(values["values"]["start"])
-                stop = float(values["values"]["stop"])
-                points = int(values["values"]["points"])
-                return True
-            except ValueError:
-                return False
-            
-        elif values["mode"].lower() == "function":
-            print("function selected")
-            print(values["values"])
-
-    def reset(self):
-        self.constant_button.setChecked(True)
-        self.show_widget(ConstantWidget)
-        self.subwidget.text_edit.clear()
-
-    @abstractmethod
-    def handle_value_change(self):
-        pass
+        try:
+            return int(self.textbox.text())
+        except ValueError as e:
+            print(f"RepetitionWidget Error: {e}")
