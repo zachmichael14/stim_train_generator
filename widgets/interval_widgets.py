81db8fe7bc934208bf0a67b784ff9021299996aa
zachmichael14 <zachmichael14@gmail.com>
2024-07-24 11:43:46 -0500

    Add frequency interval widget

--- widgets/interval_widgets.py
+++ widgets/interval_widgets.py
@@ -1,74 +1,151 @@
-from PySide6 import QtWidgets
-from widgets.basic_widgets import IntervalWidget
+from abc import abstractmethod
+
+from PySide6 import QtWidgets, QtCore
+import numpy as np
+
+from widgets.basic_widgets import ConstantWidget, RampWidget, FunctionWidget, RepetitionWidget
+
+class IntervalWidget(QtWidgets.QWidget):
+    mode_changed = QtCore.Signal()
+
+    def __init__(self, title):
+        super().__init__()
+        self.title = title
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.subwidget = ConstantWidget() # ConstantWidget is default
+        
+        # Widget mode group buttons
+        self.constant_button = QtWidgets.QRadioButton("Constant", self)
+        self.ramp_button = QtWidgets.QRadioButton("Ramp", self)
+        self.function_button = QtWidgets.QRadioButton("Function", self)
+        self.constant_button.setChecked(True)
+
+        self.subwidget_container = QtWidgets.QWidget()
+        self.subwidget_layout = QtWidgets.QVBoxLayout(self.subwidget_container)
+        self.subwidget_layout.addWidget(self.subwidget)
+
+        self.init_ui()
+        
+    def init_ui(self):
+        mode_groupbox = QtWidgets.QGroupBox(f"{self.title}")
+        mode_layout = QtWidgets.QVBoxLayout(mode_groupbox)
+        
+        self.init_mode_selectors(mode_layout)
+        mode_layout.addWidget(self.subwidget_container)
+        
+        self.main_layout.addWidget(mode_groupbox)
+
+        self.setLayout(self.main_layout)       
+
+    def init_mode_selectors(self, layout):
+        mode_selector = QtWidgets.QButtonGroup(self)
+        mode_selector.addButton(self.constant_button)
+        mode_selector.addButton(self.ramp_button)
+        mode_selector.addButton(self.function_button)
+
+        mode_selector.buttonClicked.connect(self.handle_mode_selector)
+
+        selector_layout = QtWidgets.QHBoxLayout()
+        selector_layout.addWidget(self.constant_button)
+        selector_layout.addWidget(self.ramp_button)
+        selector_layout.addWidget(self.function_button)
+
+        layout.addLayout(selector_layout)
+        
+    def handle_mode_selector(self, button: QtWidgets.QRadioButton):
+        if button == self.constant_button:
+            self.show_widget(ConstantWidget)
+        elif button == self.ramp_button:
+            self.show_widget(RampWidget)
+        else:
+            self.show_widget(FunctionWidget)
+        self.mode_changed.emit()
+
+    def show_widget(self, widget_class: QtWidgets.QWidget):
+        self.subwidget.deleteLater()
+        new_subwidget = widget_class()
+        self.subwidget_layout.replaceWidget(self.subwidget, new_subwidget)
+        self.subwidget = new_subwidget
+
+    def get_current_mode(self):
+        if self.constant_button.isChecked():
+            return "constant"
+        elif self.ramp_button.isChecked():
+            return "ramp"
+        else:
+            return "function"
+        
+    def reset(self):
+        self.constant_button.setChecked(True)
+        self.show_widget(ConstantWidget)
+        self.subwidget.text_edit.clear()
+
+    @abstractmethod
+    def get_values(self):
+        pass
+
 
 class AmplitudeIntervalWidget(IntervalWidget):
     def __init__(self):
         super().__init__("Amplitude Settings")
         self.repetition_widget = None
-        self.show_subwidget()
+        self.show_repetition_widget()
+        self.mode_changed.connect(self.show_repetition_widget)
 
-        self.mode_changed.connect(self.show_subwidget)
+    def show_repetition_widget(self):
+        if self.repetition_widget:
+            self.remove_current_subwidget()
+
+        current_mode = self.get_current_mode()
+        if current_mode == "function":
+            # No subwidget for function mode
+            return
+        
+        label_text = self.get_label_text(current_mode)
+        self.repetition_widget = RepetitionWidget(label_text)
+        self.subwidget_layout.addWidget(self.repetition_widget)
 
     def remove_current_subwidget(self):
         self.subwidget_layout.removeWidget(self.repetition_widget)
         self.repetition_widget.deleteLater()
         self.repetition_widget = None
 
-    def show_subwidget(self):
-        if self.repetition_widget:
-            self.remove_current_subwidget()
-
-        current_mode = self.get_current_mode()
-
-        if current_mode == "function":
-            # No subwidget for function mode
-            return
-
-        # Create new subwidget
-        self.repetition_widget = QtWidgets.QWidget()
-        extra_layout = QtWidgets.QHBoxLayout(self.repetition_widget)
-
-        label = QtWidgets.QLabel()
-        textbox = QtWidgets.QLineEdit()
-
+    def get_label_text(self, current_mode):
         if current_mode == "constant":
-            label.setText("Total pulses:")
-            textbox.setPlaceholderText("Total pulses")
+            label_text = "Total Pulses:"
         elif current_mode == "ramp":
-            label.setText("Pulses per Amplitude:")
-            textbox.setPlaceholderText("Pulses per Amplitude")
+            label_text = "Pulses per Amplitude:"
+        return label_text
+    
+    def get_values(self):
+        amplitudes = self.subwidget.get_values()
+        
+        repetitions = 1
+        if self.repetition_widget:
+            repetitions = self.repetition_widget.get_values()
 
+        return np.repeat(amplitudes, repetitions)
 
-        extra_layout.addWidget(label)
-        extra_layout.addWidget(textbox)
-
-        # Add new subwidget to layout
-        self.subwidget_layout.addWidget(self.repetition_widget)
-
-        # Connect textbox signal
-        textbox.textChanged.connect(self.validate_and_emit)
-
-    def handle_value_change(self):
-        values = self.get_values()
-        if self.validate_values(values):
-            self.value_changed.emit(values)
-            self.show_subwidget()
 
 class PulseLengthIntervalWidget(IntervalWidget):
     def __init__(self):
-        super().__init__("Pulse Length Settings")
+        super().__init__("Pulse Duration Settings")
 
-    def handle_value_change(self):
-        values = self.get_values()
-        if self.validate_values(values):
-            self.value_changed.emit(values)
+    def get_values(self):
+        pass
 
 
 class InterPulseIntervalWidget(IntervalWidget):
     def __init__(self):
         super().__init__("Inter-pulse Interval Settings")
 
-    def handle_value_change(self):
-        values = self.get_values()
-        if self.validate_values(values):
-            self.value_changed.emit(values)
+    def get_values(self):
+        pass
+
+
+class FrequencyWidget(IntervalWidget):
+    def __init__(self):
+        super().__init__("Frequency Settings")
+        
+    def get_values(self):
+        pass
