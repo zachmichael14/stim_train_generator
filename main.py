4787b1d0dd04e53b68c0026a93bcc0bcab4577a6
zachmichael14 <zachmichael14@gmail.com>
2024-08-01 09:12:07 -0500

    Add TODOs

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+from PySide6 import QtWidgets
+
+import manager
+import plotter
+from widgets import stim_train_widget
+
+# TODO: Adding a new train to a channel causes it to replace the previou train.
+# Appending is probably the desired behavior.
+
+# TODO: Interleave pulses. Possibly include a start latency or a way to determine order. This will probably be a DAQ function in control_window.py
+# Control_window will also no longer need create_stim_trian since that si 
+# handled by the stim manager now. It'll need access to the stim manager
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+
+    train_manager = manager.StimTrainManager()
+    train_widget = stim_train_widget.StimTrainWidget(train_manager)
+    train_plotter = plotter.StimTrainPlotter(train_manager)
+
+    window.setCentralWidget(train_widget)
+    window.setWindowTitle("Stim Train Generator")
+    window.show()
+
+    sys.exit(app.exec())
