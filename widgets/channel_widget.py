6db51245041bc748a8a30e7f47aca844521ea4e9
zachmichael14 <zachmichael14@gmail.com>
2024-08-02 15:16:04 -0500

    Inherit from BaseWidget

--- widgets/channel_widget.py
+++ widgets/channel_widget.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import Dict
 
 from PySide6 import QtWidgets
-from PySide6.QtCore import Signal
+from widgets.basic_widgets import BaseWidget
 
-class ChannelAddWidget(QtWidgets.QWidget):
+class ChannelAddWidget(QtWidgets.QWidget, BaseWidget):
     """
     Widget for selecting multiple channels via checkboxes and emitting the selected channels.
     
@@ -11,16 +11,14 @@ class ChannelAddWidget(QtWidgets.QWidget):
         channels_selected (QtCore.Signal): Signal that emits list of selected
         channels whenever channels are toggled.
     """
-    channels_selected = Signal(list)
 
     def __init__(self):
         """
         Initialize the ChannelAddWidget with checkboxes for channel selection.
         """
         super().__init__()
-        # Store checkbox references so they can be reset
-        self.checkboxes: List[QtWidgets.QCheckBox] = []
-        self.selected_channels: List[int] = []
+        # Values will be checked state
+        self.checkboxes: Dict[QtWidgets.QCheckBox: bool] = {}
         self.init_main_layout()
 
     def init_main_layout(self):
@@ -40,9 +38,24 @@ class ChannelAddWidget(QtWidgets.QWidget):
             checkbox.stateChanged.connect(lambda state, id=i: self.handle_checkbox(id, state))
 
             groupbox_layout.addWidget(checkbox)
-            self.checkboxes.append(checkbox)
+            self.checkboxes[checkbox] = checkbox.isChecked()
 
         main_layout.addWidget(groupbox)
+
+    def handle_values_edited(self, id: int, state: int):
+        """
+        Update the selected channels list based on checkbox state.
+        
+        Args:
+            id (int): The ID of the channel associated with the checkbox.
+            state (int): The state of the checkbox (checked or unchecked).
+        """
+        checkbox = self.sender()
+        print(checkbox)
+        if state == 2:  # Checked state
+            pass
+        elif id in self.selected_channels:
+            self.selected_channels.remove(id)
 
     def get_values(self):
         """
@@ -51,25 +64,12 @@ class ChannelAddWidget(QtWidgets.QWidget):
         Returns:
             list: A list of selected channel IDs.
         """
-        return self.selected_channels
-
-    def handle_checkbox(self, id: int, state: int):
-        """
-        Update the selected channels list based on checkbox state.
-        
-        Args:
-            id (int): The ID of the channel associated with the checkbox.
-            state (int): The state of the checkbox (checked or unchecked).
-        """
-        if state == 2:  # Checked state
-            self.selected_channels.append(id)
-        elif id in self.selected_channels:
-            self.selected_channels.remove(id)
+        return self.checkboxes
 
     def reset(self):
         """
         Reset all checkboxes to their default (unchecked) state and clear the selected channels list.
         """
-        for checkbox in self.checkboxes:
+        for checkbox, state in self.checkboxes.items():
             checkbox.setChecked(False)
-        self.selected_channels = []
+            state = False
