89d5d34c1a7bc9e8cb96c47d84cb1e27529a1a3a
zachmichael14 <zachmichael14@gmail.com>
2024-08-02 15:16:58 -0500

    Various refactors

--- widgets/interval_widgets.py
+++ widgets/interval_widgets.py
@@ -1,31 +1,20 @@
-from abc import abstractmethod
-
 import numpy as np
 from PySide6 import QtWidgets
-from PySide6.QtCore import Signal
 
 from widgets import basic_widgets
 
 ## TODO: Using Enum for modes might be beneficial, 
-## but it also seems to impact readability a bit. It's worth looking into more.
-
-## TODO: It also might be worth factoring out the mode_changed signal from the 
-## base class into the AmplitudeWidget class since that is the only subclass
-## that makes use of that signal.
 
 class IntervalWidget(QtWidgets.QWidget):
     """
     Base widget for selecting and displaying different interval modes.
-    
-    Attributes:
-        mode_changed (QtCore.Signal): Signal emitted when the mode is changed.
-            This is primarily used by the AmplitudeWidget subclass for
-            conditionally displaying an additional amplitude repetition widget.
-
-        subwidget (QtWidgets.QWidget): Current subwidget based on the selected
-            mode.
     """
-    mode_changed = Signal()
+    # Map modes to their respective widgets
+    MODE_MAP = {
+        "constant": basic_widgets.SingleTextFieldWidget,
+        "linear": basic_widgets.LinearWidget,
+        "function": basic_widgets.FunctionWidget,
+    }
 
     def __init__(self, title: str):
         """
@@ -36,41 +25,38 @@ class IntervalWidget(QtWidgets.QWidget):
             title (str): Title for the widget.
         """
         super().__init__()
+        # Wraps entire widget in a box labeled with title
+        self.widget_groupbox = QtWidgets.QGroupBox(f"{title}")
 
-        self.constant_button = QtWidgets.QRadioButton("Constant", self)
-        self.constant_button.setChecked(True)
-        self.linear_button = QtWidgets.QRadioButton("Linear", self)
-        self.function_button = QtWidgets.QRadioButton("Function", self)
+        # As a QButtonGroup, mode_buttons makes buttons mutually exclusive. 
+        self.mode_buttons = QtWidgets.QButtonGroup(self.widget_groupbox)
+        self.mode_buttons.buttonClicked.connect(self.handle_mode_buttons)
 
-        self.subwidget = basic_widgets.SingleTextFieldWidget()  
+        self.button_layout = QtWidgets.QHBoxLayout()
+        self.init_mode_buttons()
 
-        
-        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.subwidget = None
+        self.show_subwidget(basic_widgets.SingleTextFieldWidget)
+
+        self.groupbox_layout = QtWidgets.QVBoxLayout()
+        self.groupbox_layout.addLayout(self.button_layout)
+        self.groupbox_layout.addWidget(self.subwidget)
+        self.widget_groupbox.setLayout(self.groupbox_layout)
+
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.addWidget(self.widget_groupbox)
         self.setLayout(self.main_layout)
 
-        mode_groupbox = QtWidgets.QGroupBox(f"{title}")
-        self.mode_layout = QtWidgets.QVBoxLayout(mode_groupbox)
+    def init_mode_buttons(self):
+        for mode in self.MODE_MAP:
+            button = QtWidgets.QRadioButton(mode.capitalize())
+            self.mode_buttons.addButton(button)
+            self.button_layout.addWidget(button)
 
-        # As a QButtonGroup, mode_selector makes buttons mutually exclusive. 
-        mode_selector = QtWidgets.QButtonGroup(self)
-        mode_selector.buttonClicked.connect(self.handle_mode_selector)
-        selector_layout = QtWidgets.QHBoxLayout()
+        # Set "Constant" mode as default
+        self.mode_buttons.buttons()[0].setChecked(True)
 
-        self.button_map = {
-            self.constant_button: basic_widgets.SingleTextFieldWidget,
-            self.linear_button: basic_widgets.LinearWidget,
-            self.function_button: basic_widgets.FunctionWidget,
-        }
-
-        for button, subwidget in self.button_map.items():
-            mode_selector.addButton(button)
-            selector_layout.addWidget(button)
-
-        self.mode_layout.addLayout(selector_layout)
-        self.mode_layout.addWidget(self.subwidget)
-        self.main_layout.addWidget(mode_groupbox)
-
-    def handle_mode_selector(self, button: QtWidgets.QRadioButton):
+    def handle_mode_buttons(self):
         """
         Handle the mode change by updating the subwidget accordingly and
         emitting a signal that the mode has changed.
@@ -78,27 +64,65 @@ class IntervalWidget(QtWidgets.QWidget):
         Args:
             button (QtWidgets.QRadioButton): The button that was clicked.
         """
-        
+        mode = self.get_current_mode()
+        self.show_subwidget(self.MODE_MAP[mode])
 
-        self.show_new_subwidget(self.button_map[button])
-        self.mode_changed.emit()
-
-    def show_new_subwidget(self, widget_class: QtWidgets.QWidget):
+    def show_subwidget(self, widget_class: QtWidgets.QWidget):
         """
         Replace the current subwidget with given subwidget.
 
         Args:
             widget_class (QtWidgets.QWidget): The class of the widget to display.
-        """
-        self.subwidget.deleteLater()
-        new_subwidget = widget_class()
-        new_subwidget.values_ready_signal.connect(self.handle_values)
-        self.mode_layout.replaceWidget(self.subwidget, new_subwidget)
-        self.subwidget = new_subwidget
+        """  
+        if self.subwidget:
+            self.remove_subwidget()
 
-    def handle_values(self):
+        subwidget = widget_class()
+        subwidget.values_ready_signal.connect(self.process_values)
+        self.subwidget = subwidget
+
+    def remove_subwidget(self):
+        self.groupbox_layout.removeWidget(self.subwidget)
+        self.subwidget.deleteLater()
+        self.subwidget = None
+
+    def process_values(self):
         sender = self.sender()
 
+        function_map = {
+            self.MODE_MAP["constant"]: self.get_constant_values,
+            self.MODE_MAP["linear"]: self.get_linear_values,
+            self.MODE_MAP["function"]: self.get_function_values,
+        }
+
+        values = function_map[type(sender)]()
+        return values
+    
+    def get_constant_values(self):
+        """
+        Get values for constant mode.
+        """
+        constant_value = float(self.subwidget.get_values()['constant'])
+        return np.array([constant_value])
+
+    def get_linear_values(self):
+        """
+        Get values for linear mode.
+        """
+        values = self.subwidget.get_values()
+        start = float(values['start'])
+        stop = float(values['stop'])
+        points = int(values['points'])
+        return np.linspace(start, stop, points)
+
+    def get_function_values(self):
+        """
+        Get values for function mode.
+        Note: This is a placeholder as functions are not yet written
+        """
+        function_name = self.subwidget.get_values()['function']
+        # For now, just return an empty array
+        return np.array([])
 
     def get_current_mode(self):
         """
@@ -107,30 +131,15 @@ class IntervalWidget(QtWidgets.QWidget):
         Returns:
             str: The current mode, which can be "constant", "linear", or "function".
         """
-        if self.constant_button.isChecked():
-            return "constant"
-        elif self.linear_button.isChecked():
-            return "linear"
-        else:
-            return "function"
-        
+        return(self.mode_buttons.checkedButton().text().lower())
+
     def reset(self):
         """
         Reset the widget to its default state with "Constant" mode selected.
         """
-        self.constant_button.setChecked(True)
-        self.show_new_subwidget(basic_widgets.SingleTextFieldWidget)
+        self.mode_buttons.buttons()[0].setChecked(True)
+        self.show_subwidget(basic_widgets.SingleTextFieldWidget)
         self.subwidget.reset()
-
-    def get_values(self):
-        """
-        Retrieve subwidget values.
-
-        Returns:
-            The values from the subwidget.
-        """
-        values = self.subwidget.get_values()
-        return values
 
 
 class AmplitudeWidget(IntervalWidget):
@@ -146,32 +155,43 @@ class AmplitudeWidget(IntervalWidget):
         Initialize the AmplitudeWidget with a title and connect mode change signal.
         """
         super().__init__("Amplitude Settings")
-        self.repetition_widget = None
+        self.repetition_widget = basic_widgets.SingleTextFieldWidget("Total Pulses:")
+        self.groupbox_layout.addWidget(self.repetition_widget)
 
-        self.show_repetition_widget()
-        self.mode_changed.connect(self.show_repetition_widget)
+    def handle_mode_buttons(self):
+        """
+        Handle the mode change by updating the subwidget and repetition widgets
+        accordingly.
+        Overridden version of IntervalWidget's method.
+        """        
+        mode = self.get_current_mode()
+        self.show_subwidget(self.MODE_MAP[mode])
+        self.show_repetition_widget(mode)
 
-    def show_repetition_widget(self):
+    def show_repetition_widget(self, current_mode):
         """
         Show the repetition subwidget based on the current mode.
         """
+        # Remove preivous subwidget first so if mode is function, the method
+        # can return without a widget
         if self.repetition_widget:
             self.remove_current_subwidget()
 
-        current_mode = self.get_current_mode()
         if current_mode == "function":
-            # No subwidget for function mode
+            # No subwidget for function mode currently
+            # TODO: Eventually, this will need to display
+            # fields for entering function arguments
             return
-        
+
         label_text = self.get_label_text(current_mode)
         self.repetition_widget = basic_widgets.SingleTextFieldWidget(label_text)
-        self.mode_layout.addWidget(self.repetition_widget)
+        self.groupbox_layout.addWidget(self.repetition_widget)
 
     def remove_current_subwidget(self):
         """
         Remove and delete the current repetition subwidget.
         """
-        self.main_layout.removeWidget(self.repetition_widget)
+        self.groupbox_layout.removeWidget(self.repetition_widget)
         self.repetition_widget.deleteLater()
         self.repetition_widget = None
 
@@ -198,11 +218,11 @@ class AmplitudeWidget(IntervalWidget):
         Returns:
             np.ndarray: An array of amplitude values repeated according to the repetition settings.
         """
-        amplitudes = self.subwidget.get_values()
+        amplitudes = self.process_values()
         repetitions = 1
 
         if self.repetition_widget:
-            repetitions = self.repetition_widget.get_values()
+            repetitions = self.repetition_widget.get_values()        
         return np.repeat(amplitudes, repetitions)
     
     def reset(self):
