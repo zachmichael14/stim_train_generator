1451d4f5efb1e26dbc0bef5ad6acc0f12c7ee8c5
zachmichael14 <zachmichael14@gmail.com>
2024-08-05 11:43:17 -0500

    Process input and write to CSV

--- widgets/stim_train_widget.py
+++ widgets/stim_train_widget.py
@@ -1,25 +1,23 @@
+import csv
+
 import numpy as np
-
 from PySide6 import QtWidgets
+from PySide6.QtCore import Signal
 
-import manager
-import plotter
-from data_classes import Channel, StimTrainEvent
 from widgets import channel_widget, parameter_widgets
 
 class StimTrainWidget(QtWidgets.QWidget):
-    def __init__(self, manager: manager.StimTrainManager):
+    # Emit name of stim train csv file after it's been generated
+    signal_values_ready = Signal(str)
+    
+    def __init__(self):
         super().__init__()
-        self.events = []
-        self.manager = manager
 
         self.amplitude_widget = parameter_widgets.AmplitudeParameterWidget("Amplitude Settings")
         self.frequency_widget = parameter_widgets.StimulationParameterWidget("Frequency Settings")
         self.pulse_duration_widget = parameter_widgets.StimulationParameterWidget("Pulse Duration Settings")
         self.interpulse_interval_widget = parameter_widgets.StimulationParameterWidget("Inter-pulse Interval Settings")
         self.channel_add_widget = channel_widget.ChannelAddWidget()
-
-        self.plotter = plotter.StimTrainPlotter(self.manager)
 
         self.init_main_layout()
         
@@ -48,102 +46,71 @@ class StimTrainWidget(QtWidgets.QWidget):
 
         channels = self.channel_add_widget.get_active_channels()
 
-        pulse_durations = self.pulse_duration_widget.input_values
-        frequencies = self.frequency_widget.input_values
-        intervals = self.interpulse_interval_widget.input_values
+        pulse_durations = self.pad_input_array(self.pulse_duration_widget,
+                                               train_length) 
+               
+        frequencies = self.pad_input_array(self.frequency_widget,
+                                           train_length)
+        
+        intervals = self.pad_input_array(self.interpulse_interval_widget,
+                                         train_length)
 
-        # The current issue is tat non amplitude parameters, if they are constant, are shorter arrays than the ampltude array, so indexin doent work
+        filename = "test.csv"
+        with open(filename, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Channel ID", "Amplitude", "Frequency", "Start Time", "End Time"])
 
-        current_time = 0
-        for i, channel_id in enumerate(channels):
-            channel = Channel(channel_id, [])
+            current_time = 0
+            for i, channel_id in enumerate(channels):
+                for j in range(train_length):
+                    amplitude = amplitudes[j]
+                    frequency = frequencies[j]
+                    duration = pulse_durations[j]
 
-            for j in range(train_length):
-                amplitude = amplitudes[j]
-                frequency = frequencies[j]
-                duration = pulse_durations[j]
+                    start_time = current_time
+                    end_time = start_time + duration
 
-                start_time = current_time
-                end_time = start_time + duration
-
-                stim_event = StimTrainEvent(
-                    amplitude,
-                    start_time,
-                    end_time,
-                    frequency,
-                )
-                channel.events.append(stim_event)
-                self.events.append(stim_event)
+                    writer.writerow([channel_id,
+                                     amplitude,
+                                     frequency,
+                                     start_time,
+                                     end_time,
+                                     ])
             
-                # Update current_time for the interval event
-                current_time = end_time
+                    # Update current_time for the interval event
+                    current_time = end_time
                 
-                # Calculate start and end times for the interval event
-                interval_start_time = current_time
-                interval_end_time = interval_start_time + intervals[j]
-            
-                interval_event = StimTrainEvent(
-                    0,
-                    interval_start_time,
-                    interval_end_time,
-                    0,
-                )
-                channel.events.append(interval_event)
-                self.events.append(interval_event)
-            
-                # Update current_time for the next event
-                current_time = interval_end_time
+                    # Calculate start and end times for the interval event
+                    interval_start_time = current_time
+                    interval_end_time = interval_start_time + intervals[j]
 
-            self.manager.add_channel(channel)
-            
+
+                    writer.writerow([channel_id,
+                                     0, # Intervals don't have amplitudes
+                                     0, # Intervals don't have frequency
+                                     interval_start_time,
+                                     interval_end_time,
+                                     ])
+                                
+                    # Update current_time for the next event
+                    current_time = interval_end_time
+
+        self.signal_values_ready.emit(filename)
         self.reset_to_default()
-        self.show_plotter()
 
-    def show_plotter(self):
-        self.plotter.plot_data()
-        self.plotter.show()
+    def pad_input_array(self, widget, desired_length):
+        values = widget.input_values
 
-    def get_pulse_durations(self, number_of_pulses):
-        pulse_durations = self.pulse_duration_widget.get_values()
+        if widget.get_current_mode() == "constant":
+            values = np.repeat(values, desired_length)   
 
-        if self.pulse_duration_widget.get_current_mode() == "constant":
-            pulse_durations = np.repeat(pulse_durations, number_of_pulses)
-
-        if len(pulse_durations) != number_of_pulses:
-            print("PulseDurationWidget Error: too many or too few pulse duration values")
-            print(f"Actual length of durations list: {len(pulse_durations)}")
-            print(f"Expected length of durations list: {number_of_pulses}")
+        if len(values) != desired_length:
+            print("Error: Too many or too few pulse values")
+            print(f"Actual length of values list: {values.size}")
+            print(f"Expected length of values list: {desired_length}")
             raise Exception
         
-        return pulse_durations
-
-    def get_frequencies(self, number_of_pulses):
-        frequencies = self.frequency_widget.get_values()
-
-        if self.frequency_widget.get_current_mode() == "constant":
-            frequencies = np.repeat(frequencies, number_of_pulses)
-
-        if len(frequencies) != number_of_pulses:
-            print("FrequencyWidget Error: too many or too few frequecy values")
-            print(f"Actual length of frequency list: {len(frequencies)}")
-            print(f"Expected length of frequency list: {number_of_pulses}")
-            raise Exception
-        
-        return frequencies
-    
-    def get_inter_pulse_intervals(self, number_of_pulses):
-        intervals = self.interpulse_interval_widget.get_values()
-
-        if self.interpulse_interval_widget.get_current_mode() == "constant":
-            intervals = np.repeat(intervals, number_of_pulses)
-
-        if len(intervals) != number_of_pulses:
-            print("FrequencyWidget Error: too many or too few frequecy values")
-            print(f"Actual length of frequency list: {len(intervals)}")
-            print(f"Expected length of frequency list: {number_of_pulses}")
-            raise Exception
-        
-        return intervals
+        return values
 
     def reset_to_default(self):
         self.amplitude_widget.reset()
