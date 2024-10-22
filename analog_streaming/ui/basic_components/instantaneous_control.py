879ee604c3592394ba6e78b36c396773d5577c53
zachmichael14 <zachmichael14@gmail.com>
2024-10-22 14:08:25 -0500

    Add toggleable pause button

--- analog_streaming/ui/basic_components/instantaneous_control.py
+++ analog_streaming/ui/basic_components/instantaneous_control.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Signal
 from PySide6.QtWidgets import (
-    QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QGroupBox,
+    QWidget, QVBoxLayout, QFormLayout, QPushButton, QGroupBox, QToolButton
 )
 
 from .debounced_spin_box import DebouncedDoubleSpinBox
@@ -9,68 +9,70 @@ from ...utils.defaults import StimDefaults
 
 
 class InstantaneousControlWidget(QWidget):
-    signal_update_mode_changed = Signal(bool)
     signal_on_off_changed = Signal(bool)
+    signal_pause_toggled = Signal(bool)
+    signal_update_mode_changed = Signal(bool)
     signal_frequency_changed = Signal(float)
     signal_amplitude_changed = Signal(float)
+    signal_toggle_thread_pause = Signal(bool)
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        main_layout = QVBoxLayout()
+        
+        self.onoff_button = SlideToggle()
+        self.live_update_toggle = SlideToggle()
 
-        group_box = QGroupBox("Instantaneous Control")
-        group_box_layout = QVBoxLayout()
-
-        freq_layout = QHBoxLayout()
-        freq_label = QLabel("Frequency (Hz)")
         self.freq_spinbox = DebouncedDoubleSpinBox(max_change_per_step=StimDefaults.FrequencyDefaults.MAX_INCREMENT_SIZE)
-        self.freq_spinbox.valueChangedFinished.connect(self._handle_frequency_changed)
+        self.amp_spinbox = DebouncedDoubleSpinBox(max_change_per_step=StimDefaults.AmplitudeDefaults.MAX_INCREMENT_SIZE)
+        
+        self.send_update_button = QPushButton("Send Updates")
+        self.pause_button = QToolButton()
+
+        self._setup_ui()
+        self._set_widget_settings()
+        self._connect_signals()
+
+    def _setup_ui(self):
+        main_layout = QVBoxLayout()
+        group_box = QGroupBox("Instantaneous Control")
+        form_layout = QFormLayout()
+
+        form_layout.addRow("Stimulation:", self.onoff_button)
+        form_layout.addRow("Live Update:", self.live_update_toggle)
+        form_layout.addRow("Frequency (Hz):", self.freq_spinbox)
+        form_layout.addRow("Amplitude (mA):", self.amp_spinbox)
+        form_layout.addRow(self.send_update_button)
+        form_layout.addRow(self.pause_button)
+
+        group_box.setLayout(form_layout)
+        main_layout.addWidget(group_box)
+        self.setLayout(main_layout)
+
+    def _set_widget_settings(self):
         self.freq_spinbox.setRange(0, StimDefaults.FrequencyDefaults.GLOBAL_MAXIMUM)
         self.freq_spinbox.setValue(StimDefaults.FrequencyDefaults.GLOBAL_FREQUENCY)
 
-        freq_layout.addWidget(freq_label)
-        freq_layout.addWidget(self.freq_spinbox)
+        self.amp_spinbox.setRange(0, StimDefaults.AmplitudeDefaults.GLOBAL_MAXIMUM)
+        self.amp_spinbox.setValue(StimDefaults.AmplitudeDefaults.GLOBAL_AMPLITUDE)
 
-        amp_layout = QHBoxLayout()
-        amp_label = QLabel("Amplitude (mA)")
-        self.amp_spinbox = DebouncedDoubleSpinBox(max_change_per_step=StimDefaults.AmplitudeDefaults.MAX_INCREMENT_SIZE)
+        self.pause_button.setText("Pause Thread")
+        # QToolButtons are slightly smaller than QPushButtons
+        self.pause_button.setSizePolicy(self.send_update_button.sizePolicy())
+        self.pause_button.setCheckable(True) 
+
+    def _connect_signals(self):
+        self.onoff_button.signal_toggled.connect(self.signal_on_off_changed.emit)
+        self.live_update_toggle.signal_toggled.connect(self._handle_update_mode_changed)
+
+        self.freq_spinbox.valueChangedFinished.connect(self._handle_frequency_changed)
         self.amp_spinbox.valueChangedFinished.connect(self._handle_amplitude_changed)
 
-        self.amp_spinbox.setRange(0, StimDefaults.AmplitudeDefaults.GLOBAL_MAXIMUM)
-        self.amp_spinbox.setValue(0)
-        amp_layout.addWidget(amp_label)
-        amp_layout.addWidget(self.amp_spinbox)
+        self.send_update_button.clicked.connect(self._handle_update_button_clicked)
+        self.pause_button.clicked.connect(self._handle_pause_button_clicked)
 
-        on_off_layout = QHBoxLayout()
-        onoff_label = QLabel("Stimulation:")
-        onoff_button = SlideToggle()
-        onoff_button.signal_toggled.connect(self.signal_on_off_changed.emit)
-        on_off_layout.addWidget(onoff_label)
-        on_off_layout.addWidget(onoff_button)
-
-        update_layout = QHBoxLayout()
-        update_label = QLabel("Live Update:")
-        update_button = SlideToggle()
-        update_button.signal_toggled.connect(self._handle_update_mode_changed)
-        update_layout.addWidget(update_label)
-        update_layout.addWidget(update_button)
-
-        pause_button = QPushButton("Pause")
-        self.update_button = QPushButton("Send Updates")
-        self.update_button.clicked.connect(self._handle_update_button_clicked)
-
-        group_box_layout.addLayout(on_off_layout)
-        group_box_layout.addLayout(update_layout)
-
-        group_box_layout.addLayout(freq_layout)
-        group_box_layout.addLayout(amp_layout)
-        group_box_layout.addWidget(self.update_button)
-        group_box_layout.addWidget(pause_button)
-
-        group_box.setLayout(group_box_layout)
-        main_layout.addWidget(group_box)
-        
-        self.setLayout(main_layout)
+    def _handle_pause_button_clicked(self, is_paused: bool):
+        self.signal_pause_toggled.emit(is_paused)
+        self.pause_button.setText("Resume Thread" if is_paused else "Pause Thread")
 
     def _handle_update_button_clicked(self):
         self.signal_update_mode_changed.emit(True)
@@ -87,8 +89,6 @@ class InstantaneousControlWidget(QWidget):
         self.signal_amplitude_changed.emit(value)
 
     def _handle_update_mode_changed(self, are_updates_live: bool):
-        if not are_updates_live:
-            self.update_button.show()
-        else:
-            self.update_button.hide()
         self.signal_update_mode_changed.emit(are_updates_live)
+        self.send_update_button.setEnabled(not are_updates_live)
+        self.send_update_button.setText("Updates Are Live" if are_updates_live else "Send Updates")
