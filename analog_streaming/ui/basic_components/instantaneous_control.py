99ad4c2971103bd8e99ecc93cb32731257191fa0
zachmichael14 <zachmichael14@gmail.com>
2024-10-29 09:16:59 -0500

    Remove amplitude and frequency settings

--- analog_streaming/ui/basic_components/instantaneous_control.py
+++ analog_streaming/ui/basic_components/instantaneous_control.py
@@ -22,8 +22,8 @@ class InstantaneousControlWidget(QWidget):
         self.onoff_button = SlideToggle()
         self.live_update_toggle = SlideToggle()
 
-        self.freq_spinbox = DebouncedDoubleSpinBox(max_increase=StimDefaults.FrequencyDefaults.MAX_INCREMENT_SIZE)
-        self.amp_spinbox = DebouncedDoubleSpinBox(max_increase=StimDefaults.AmplitudeDefaults.MAX_INCREMENT_SIZE)
+        # self.freq_spinbox = DebouncedDoubleSpinBox(max_increase=StimDefaults.FrequencyDefaults.MAX_INCREMENT_SIZE)
+        # self.amp_spinbox = DebouncedDoubleSpinBox(max_increase=StimDefaults.AmplitudeDefaults.MAX_INCREMENT_SIZE)
         
         self.send_update_button = QPushButton("Send Updates")
         self.pause_button = QToolButton()
@@ -39,8 +39,8 @@ class InstantaneousControlWidget(QWidget):
 
         form_layout.addRow("Stimulation:", self.onoff_button)
         form_layout.addRow("Live Update:", self.live_update_toggle)
-        form_layout.addRow("Frequency (Hz):", self.freq_spinbox)
-        form_layout.addRow("Amplitude (mA):", self.amp_spinbox)
+        # form_layout.addRow("Frequency (Hz):", self.freq_spinbox)
+        # form_layout.addRow("Amplitude (mA):", self.amp_spinbox)
         form_layout.addRow(self.send_update_button)
         form_layout.addRow(self.pause_button)
 
@@ -49,11 +49,11 @@ class InstantaneousControlWidget(QWidget):
         self.setLayout(main_layout)
 
     def _set_widget_settings(self):
-        self.freq_spinbox.setRange(0, StimDefaults.FrequencyDefaults.GLOBAL_MAXIMUM)
-        self.freq_spinbox.setValue(StimDefaults.FrequencyDefaults.GLOBAL_FREQUENCY)
+        # self.freq_spinbox.setRange(0, StimDefaults.FrequencyDefaults.GLOBAL_MAXIMUM)
+        # self.freq_spinbox.setValue(StimDefaults.FrequencyDefaults.GLOBAL_FREQUENCY)
 
-        self.amp_spinbox.setRange(0, StimDefaults.AmplitudeDefaults.GLOBAL_MAXIMUM)
-        self.amp_spinbox.setValue(StimDefaults.AmplitudeDefaults.GLOBAL_AMPLITUDE)
+        # self.amp_spinbox.setRange(0, StimDefaults.AmplitudeDefaults.GLOBAL_MAXIMUM)
+        # self.amp_spinbox.setValue(StimDefaults.AmplitudeDefaults.GLOBAL_AMPLITUDE)
 
         self.pause_button.setText("Pause Thread")
         # QToolButtons are slightly smaller than QPushButtons
@@ -64,8 +64,8 @@ class InstantaneousControlWidget(QWidget):
         self.onoff_button.signal_toggled.connect(self._handle_on_off_toggle)
         self.live_update_toggle.signal_toggled.connect(self._handle_update_mode_changed)
 
-        self.freq_spinbox.signal_value_changed.connect(self._handle_frequency_changed)
-        self.amp_spinbox.signal_value_changed.connect(self._handle_amplitude_changed)
+        # self.freq_spinbox.signal_value_changed.connect(self._handle_frequency_changed)
+        # self.amp_spinbox.signal_value_changed.connect(self._handle_amplitude_changed)
 
         self.send_update_button.clicked.connect(self._handle_update_button_clicked)
         self.pause_button.clicked.connect(self._handle_pause_button_clicked)
@@ -79,17 +79,17 @@ class InstantaneousControlWidget(QWidget):
 
     def _handle_update_button_clicked(self):
         self.signal_update_mode_changed.emit(True)
-        self._handle_frequency_changed()
-        self._handle_amplitude_changed()
+        # self._handle_frequency_changed()
+        # self._handle_amplitude_changed()
         self.signal_update_mode_changed.emit(False)
 
-    def _handle_frequency_changed(self):
-        value = self.freq_spinbox.value()
-        self.signal_frequency_changed.emit(value)
+    # def _handle_frequency_changed(self):
+    #     value = self.freq_spinbox.value()
+    #     self.signal_frequency_changed.emit(value)
 
-    def _handle_amplitude_changed(self):
-        value = self.amp_spinbox.value()
-        self.signal_amplitude_changed.emit(value)
+    # def _handle_amplitude_changed(self):
+    #     value = self.amp_spinbox.value()
+    #     self.signal_amplitude_changed.emit(value)
 
     def _handle_update_mode_changed(self, are_updates_live: bool):
         self.signal_update_mode_changed.emit(are_updates_live)
