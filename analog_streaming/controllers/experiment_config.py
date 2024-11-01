a6ddbbf50300fbf21a1b73c331f6513890a7b99a
zachmichael14 <zachmichael14@gmail.com>
2024-10-31 20:37:54 -0500

    Imports reflect new dir structure

--- analog_streaming/controllers/experiment_config.py
+++ analog_streaming/controllers/experiment_config.py
@@ -8,8 +8,8 @@ from PySide6.QtWidgets import (
 
 from analog_streaming.core.defaults import ConfigDefaults
 from analog_streaming.managers.config_manager import ConfigManager
-from analog_streaming.ui.basic_components.sensor_confirmation import SensorVisualizationWidget
-from analog_streaming.ui.composite_widgets.config_widget import ConfigWidget
+from analog_streaming.widgets.basic_components.sensor_confirmation import SensorVisualizationWidget
+from analog_streaming.widgets.composite_widgets.config_widget import ConfigWidget
 
 class ExperimentConfigWindow(QMainWindow):
     """Manages interactions between config widget and config manager."""
